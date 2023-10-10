242b7e0e876cc841d2a28e3a93c5194c53cd3adf
gaurav.tambatkar <gaurav.tambatkar@ril.com>
2023-10-11 00:07:19 +0530

    fix: code commit fix

--- Workspace/node/node_project/config/development.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  APP_URI: "https://localhost/",
-  SMS_AUTH_KEY:"293277AnmBNvl8U4Ok5d762977",
-  // SMS_AUTH_KEY:"324673A2Hiq3gcd5e7b6e3bP1",
-  SMS_API_URL:"http://smsp.myoperator.co/api"
-};
